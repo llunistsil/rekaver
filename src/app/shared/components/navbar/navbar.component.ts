028db8206f526aff32911540b8e2f48941b7ef64
R1505-We2-6-Stud <suvorovka04@gmail.com>
2024-02-26 18:07:04 +0300

    Nav bar + init home page

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
 import {Component} from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
-import {NgIconComponent} from "@ng-icons/core";
+import {NgIcon, NgIconComponent, provideIcons} from "@ng-icons/core";
+import {heroHomeSolid, heroPhoneSolid, heroPhotoSolid, heroSquares2x2Solid} from "@ng-icons/heroicons/solid";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: 'navbar.component.html',
   imports: [
     NgOptimizedImage,
-    NgIconComponent
+    NgIconComponent,
+    NgIcon
   ],
-  standalone: true
+  standalone: true,
+  viewProviders: [provideIcons({heroHomeSolid,heroSquares2x2Solid,heroPhotoSolid,heroPhoneSolid})]
 })
 
 export class NavbarComponent{
